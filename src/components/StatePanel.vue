3355cd5e3c3eae4ef5f699fca371e8f7bc94d55b
Henry Voigt <oigty69@freenet.de>
2024-03-19 07:26:52 +0100

    added state panel

---
+++ src/components/StatePanel.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useUiStore } from '@/stores/ui.js';
+import { useAppControllerStore  } from '@/stores/appController';
+import { useTypingContentStore  } from '@/stores/typingContent';
+
+const uiStore = useUiStore();
+const appControllerStore = useAppControllerStore();
+const typingContentStore =  useTypingContentStore();
+
+</script>
+
+<template>
+  <div
+    class="box-1 q-ma-xs"
+    :style="{'height': uiStore.heightPanel + 'px'}"
+  >
+    <div class="row">
+      <div class="col-6">
+        .. {{ appControllerStore.currentPositionBlock }} // {{ appControllerStore.currentPositionChar }}
+      </div>
+      <div class="col-6">
+        ..
+      </div>
+    </div>
+  </div>
+</template>
