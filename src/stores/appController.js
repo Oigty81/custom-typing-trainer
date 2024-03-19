783f9fd659c89f61136e10e76fadf37894b89c70
Henry Voigt <oigty69@freenet.de>
2024-03-19 07:29:05 +0100

    update appcontroller store

--- src/stores/appController.js
+++ src/stores/appController.js
@@ -1,16 +1,93 @@
-import { defineStore, storeToRefs } from 'pinia';
-import { ref, computed, watch } from 'vue';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
-import { useKeyboardStore  } from '@/stores/keyboard';
+import { useAppStateStore  } from '@/stores/appState';
+import { useTypingContentStore  } from '@/stores/typingContent';
 
 export const useAppControllerStore = defineStore('appControllerStore', () => {
 
-    const keyboardStore = storeToRefs(useKeyboardStore());
+    const  appState =  useAppStateStore();
+    const  typingContentStore =  useTypingContentStore();
 
-    watch(keyboardStore.lastPressedKey, () => {
-        console.log('runs', keyboardStore.lastPressedKey.value);
+    const currentPositionBlock = ref (0);
+    const currentPositionChar = ref (0);
+
+    const keypressHandler = (e) => {
+        if(appState.typingProgressEnabled) {
+
+            if(typingContentStore.contentData.length === 0) {
+                return;
+            }
+
+            progressTypingCheck(e.key);
+        };
+    };
+
+    const progressTypingCheck = (key) => {
+        console.log('current key', key); //TODO: remove later
+
+        if(isTypingContentEndPosition()) {
+            return;
+        }
+        while(
+            typingContentStore.contentData[currentPositionBlock.value].type === 'lf' ||
+            typingContentStore.contentData[currentPositionBlock.value].type === 'none') {
+                currentPositionBlock.value++;
+                if(isTypingContentEndPosition()) {
+                    return;
+                }
+        }
         
-    }, {immediate: true, deep: false});
+        let currentCharTocheck = "";
 
-    return {};
+        if(appState.ignoreCapitalizeEnabled) {
+                        
+            currentCharTocheck =  typingContentStore.contentData[currentPositionBlock.value].chars[currentPositionChar.value].char.toLowerCase();
+        } else {
+            currentCharTocheck =  typingContentStore.contentData[currentPositionBlock.value].chars[currentPositionChar.value].char;
+        }
+
+        typingContentStore.contentData[currentPositionBlock.value].chars[currentPositionChar.value].keypressTimeStamp = new Date();
+        console.log('curr:',key,':', currentPositionBlock.value, currentPositionChar.value);
+        if(currentCharTocheck === key) {
+            typingContentStore.contentData[currentPositionBlock.value].chars[currentPositionChar.value].failed = false;
+            if(appState.keySoundEnabled) {
+                let audio = new Audio('./good.mp3');
+                audio.play();
+            }
+        } else {
+            let failedChar = "";
+            if(appState.ignoreCapitalizeEnabled) {
+                failedChar = key.toLowerCase();
+            } else {
+                failedChar = key.value;
+            }
+            typingContentStore.contentData[currentPositionBlock.value].chars[currentPositionChar.value].failed = true;
+            typingContentStore.contentData[currentPositionBlock.value].chars[currentPositionChar.value].failedChar = failedChar;
+            if(appState.keySoundEnabled) {
+                let audio = new Audio('./bad.mp3');
+                audio.play();
+            }
+        }
+
+        if((currentPositionChar.value + 1) === typingContentStore.contentData[currentPositionBlock.value].chars.length) {
+            currentPositionBlock.value++;
+            currentPositionChar.value = 0;
+        } else {
+            currentPositionChar.value++;
+        }
+    };
+
+    const isTypingContentEndPosition = () => {
+        let isContentBlockEnding =
+            typingContentStore.contentData.length - 1 == currentPositionBlock.value ? true : false;
+        let isContentCharEnding =
+            typingContentStore.contentData[currentPositionBlock.value].chars.length - 1 == currentPositionChar.value ? true : false;
+            
+        return isContentBlockEnding && isContentCharEnding;
+    };
+    
+    window.addEventListener('keypress', keypressHandler);
+
+    return { currentPositionBlock, currentPositionChar };
 });
