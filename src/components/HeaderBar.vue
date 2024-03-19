61e1293f9eaa59a9454c1194b0b51c4714cc0cf5
Henry Voigt <oigty69@freenet.de>
2024-03-19 07:27:46 +0100

    added headerbar

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+// ...
+
+</script>
+
+<template>
+  <q-header
+    elevated
+    class="bg-primary text-white"
+  >
+    <q-toolbar>
+      <q-toolbar-title class="non-selectable">
+        <q-avatar>
+          <img
+            src="@/assets/icon.png"
+            style="width: 80%; height: auto;"
+          >
+        </q-avatar>
+        <span class="q-mx-md">Typing Trainer</span>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
