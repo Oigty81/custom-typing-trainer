241b0a2812bbfd343da85dae29c4a7d52286f4ce
Henry Voigt <oigty69@freenet.de>
2024-03-19 07:27:30 +0100

    added control panel

---
+++ src/components/ControlPanel.vue
@@ -0,0 +1,170 @@
+<script setup>
+import { ref, computed, watch } from 'vue';
+import { useQuasar } from 'quasar';
+
+import { useUiStore } from '@/stores/ui.js';
+import { useAppStateStore } from '@/stores/appState';
+import { useAppControllerStore } from '@/stores/appController';
+import { useTypingContentStore  } from '@/stores/typingContent';
+
+const $q = useQuasar();
+
+const uiStore = useUiStore();
+const appStateStore =  useAppStateStore();
+const appControllerStore =  useAppControllerStore();
+const typingContentStore =  useTypingContentStore();
+
+const fileToUpload = ref(null);
+
+
+
+const resetProgress = () => {
+  appControllerStore.currentPositionBlock = 0;
+  appControllerStore.currentPositionChar = 0;
+  typingContentStore.removeProgressDataFromContentData();
+};
+
+const clearContent = () => {
+  appControllerStore.currentPositionBlock = 0;
+  appControllerStore.currentPositionChar = 0;
+  typingContentStore.contentData = [];
+};
+
+const loadDemoFile = (filename) => {
+  appStateStore.typingProgressEnabled = false;
+  typingContentStore.loadDemoContent(filename)
+  .then(() => {
+    resetProgress();
+    appStateStore.typingProgressEnabled = true;
+  })
+  .catch((err) => {
+    $q.notify({
+      progress: true,
+      message: "no valid text file!",
+      color: 'negative',
+      position: 'top-right'
+    });
+    console.log('was error: ', err.error);
+  });
+};
+
+watch(fileToUpload, () => {
+  appStateStore.typingProgressEnabled = false;
+  if(fileToUpload.value != null) {
+    typingContentStore.loadContentFromCustomFile(fileToUpload.value)
+    .then(() => {
+      resetProgress();
+      appStateStore.typingProgressEnabled = true;
+      fileToUpload.value = null;
+    })
+    .catch(() => {
+      appStateStore.typingProgressEnabled = true;
+      fileToUpload.value = null;
+      $q.notify({
+            progress: true,
+            message: "no valid text file!",
+            color: 'negative',
+            position: 'top-right'
+          });
+    });
+  }
+  }, {deep: false, immediate: false }
+);
+</script>
+
+<template>
+  <div
+    class="box-1 q-ma-xs"
+    :style="{'height': uiStore.heightPanel + 'px'}"
+  >
+    <div class="row q-mt-lg">
+      <div class="col-3">
+        <div class="row">
+          <div class="col-12 q-px-sm">
+            <q-btn
+              no-caps
+              size="sm"
+              style="width: 100%;"
+              @click="resetProgress()"
+            >
+              Reset Progress
+            </q-btn>
+          </div>
+          <div class="col-12 q-px-sm q-pt-sm">
+            <q-btn
+              no-caps
+              size="sm"
+              style="width: 100%;"
+              @click="clearContent()"
+            >
+              Clear Content
+            </q-btn>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-3 offset-3 q-px-sm">
+        <q-btn
+          no-caps
+          size="sm"
+          style="width: 100%;"
+          @click="loadDemoFile('demotext.txt')"
+        >
+          Load Demo 1
+        </q-btn>
+      </div>
+      <div class="col-3 q-px-sm">
+        <q-btn
+          no-caps
+          size="sm"
+          style="width: 100%;"
+          @click="loadDemoFile('demotext2.txt')"
+        >
+          Load Demo 2
+        </q-btn>
+      </div>
+    </div>
+    <div class="row q-mt-md">
+      <div class="col-6">
+        <div class="row">
+          <div class="col-12">
+            <q-checkbox
+              v-model="appStateStore.ignoreCapitalizeEnabled"
+              label="ignore capitalize"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <q-checkbox
+              v-model="appStateStore.keySoundEnabled"
+              label="keysound enabled"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-file
+          v-model="fileToUpload"
+          clearable
+          class="filepicker-1"
+          filled
+          label="choose txt-file for training"
+          accept=".txt"
+        >
+          <template #prepend>
+            <q-icon
+              v-if="fileToUpload === null || fileToUpload === undefined"
+              name="attach_file"
+            />
+            <span
+              v-if="fileToUpload === null || fileToUpload === undefined"
+              class="text-subtitle2"
+              @click.stop
+            />
+          </template>
+        </q-file>
+      </div>
+    </div>
+  </div>
+</template>
