cd75a01d316c2f16ed335bb87daf0cd61f6aaa01
Henry Voigt <oigty69@freenet.de>
2024-04-05 01:32:03 +0200

    remove unnecessary console.log() calls

--- src/stores/typingContent.js
+++ src/stores/typingContent.js
@@ -1,11 +1,8 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
-
 export const useTypingContentStore = defineStore('typingContentStore', () => {
-
     const contentData = ref([]);
-    
     const loadDemoContent = async (filename) => {
         return new Promise((resolve, reject) => {
             fetch(filename)
@@ -19,17 +16,17 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
                 let isText = true;
                
                 response.text().then((loadedText) => {
-                    let utf8Encode = new TextEncoder();
+                    //TODO: refactore to utilities "fileHelper.js ("check is text")
+                    let utf8Encode = new TextEncoder(/*"utf-8"*/);
                     let loadedTextBytes = utf8Encode.encode(loadedText);
                     loadedTextBytes.forEach(ltb => {
-                        if(ltb > 127) {
+                        if(ltb === 0) {
                             isText = false;
                             return;
                         }
                     });
-                    
                     if(isText) {
-                        contentData.value = generateContentData(loadedTextBytes);
+                        contentData.value = generateContentData(new TextDecoder().decode(loadedTextBytes));
                         resolve();
                     } else {
                         contentData.value = [];
@@ -56,17 +53,18 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
 
           getTextFile(file)
           .then((txtdata) => {
+            //TODO: refactore to utilities "fileHelper.js" ("check is text")
               let loadedTextBytes = new Uint8Array(txtdata);
-              console.log("bb", loadedTextBytes);
               loadedTextBytes.forEach(ltb => {
-                if(ltb > 127) {
+                if(ltb === 0) {
                     isText = false;
                     return;
                 }
               });
               
               if(isText) {
-                contentData.value = generateContentData(loadedTextBytes);
+
+                contentData.value = generateContentData(new TextDecoder().decode(loadedTextBytes));
                 resolve();
               } else {
                   contentData.value = [];
@@ -79,7 +77,7 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
         });
     };
 
-
+    //TODO: remove to utility "fileHelper.js"
     const getTextFile = async (file) => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
@@ -92,12 +90,21 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
     };
 
 
+    //TODO: refactor to utilities "typing.js" OR "content.js"
+    function isLetter(str) {
+        return str.length === 1 && str.match(/[a-z]/i);
+    }
 
+    //TODO: refactor to utilities "typing.js" OR "content.js"
     const getContentType = (char) => {
-        let type = "char";
+        let type = "anychar";
+        if(isLetter(char) !== null) {
+            type = "letter";
+        }
+
         if(char === '\r') {
             type = "none";
-         };
+        }
         if(char === '\n') {
             type = "lf";
         };
@@ -109,16 +116,18 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
 
     };
 
-    const generateContentData = (loadedTextBytes) => {
+    //TODO: refactor to utilities "typing.js" OR "content.js"
+    const generateContentData = (contentString) => {
         let dataObject = [];
         let lastContentType = null;
         let contentBlock = {};
-
-        loadedTextBytes.forEach((b, i) => {
-            let char = String.fromCharCode(b);
+       
+        for(let i = 0; i<contentString.length;i++) {
+            let char = contentString[i];
             let type = getContentType(char);
 
-            char = type === 'char' ? char
+            char = type === 'anychar' ? char
+                :  type === 'letter' ? char
                 : type === 'space' ? '_' : '';
                 
             if(i === 0 || lastContentType !== type) {
@@ -136,7 +145,6 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
                     char: char,
                     failed: null,
                     failedChar: "",
-                    keypressTimeStamp: null
                 });
 
             } else {
@@ -145,22 +153,35 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
                     char: char,
                     failed: null,
                     failedChar: "",
-                    keypressTimeStamp: null
                 });
             }
-        });
+        }
 
         dataObject = dataObject.filter(item => item.type != 'none');
         
+        //TODO: find better solution for last char to trigger finish typing
+        let lastObject = {
+            type: 'space',
+            chars: []
+        };
+
+        lastObject.chars.push({
+            char: ".",
+            failed: null,
+            failedChar: "",
+        });
+
+        dataObject.push(lastObject);
+
         return dataObject;
     };
 
     const removeProgressDataFromContentData = () => {
+        //TODO: call an utility function "typing.js" OR "content.js"
         contentData.value.forEach(cd => {
             cd.chars.forEach(ch => {
                 ch.failed = null;
                 ch.failedChar = "";
-                ch.keypressTimeStamp = null;
             });
         });
     };
