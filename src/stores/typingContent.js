eac5b41de6198aaf73b052e064430a08d10dcdb8
Henry Voigt <oigty69@freenet.de>
2024-04-05 01:46:40 +0200

    refactoring: added utilities for manage content

--- src/stores/typingContent.js
+++ src/stores/typingContent.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+import { generateContentData, removeProgressData } from '@/utilities/content';
+
 export const useTypingContentStore = defineStore('typingContentStore', () => {
     const contentData = ref([]);
     const loadDemoContent = async (filename) => {
@@ -89,101 +91,8 @@ export const useTypingContentStore = defineStore('typingContentStore', () => {
         });
     };
 
-
-    //TODO: refactor to utilities "typing.js" OR "content.js"
-    function isLetter(str) {
-        return str.length === 1 && str.match(/[a-z]/i);
-    }
-
-    //TODO: refactor to utilities "typing.js" OR "content.js"
-    const getContentType = (char) => {
-        let type = "anychar";
-        if(isLetter(char) !== null) {
-            type = "letter";
-        }
-
-        if(char === '\r') {
-            type = "none";
-        }
-        if(char === '\n') {
-            type = "lf";
-        };
-        if(char === ' ' || char === '\t') {
-            type = "space";
-        };
-
-        return type;
-
-    };
-
-    //TODO: refactor to utilities "typing.js" OR "content.js"
-    const generateContentData = (contentString) => {
-        let dataObject = [];
-        let lastContentType = null;
-        let contentBlock = {};
-       
-        for(let i = 0; i<contentString.length;i++) {
-            let char = contentString[i];
-            let type = getContentType(char);
-
-            char = type === 'anychar' ? char
-                :  type === 'letter' ? char
-                : type === 'space' ? '_' : '';
-                
-            if(i === 0 || lastContentType !== type) {
-                if(i !== 0) {
-                    dataObject.push(contentBlock);
-                }
-                lastContentType = type;
-
-                contentBlock = {
-                    type: type,
-                    chars: []
-                };
-
-                contentBlock.chars.push({
-                    char: char,
-                    failed: null,
-                    failedChar: "",
-                });
-
-            } else {
-                lastContentType = type;
-                contentBlock.chars.push({
-                    char: char,
-                    failed: null,
-                    failedChar: "",
-                });
-            }
-        }
-
-        dataObject = dataObject.filter(item => item.type != 'none');
-        
-        //TODO: find better solution for last char to trigger finish typing
-        let lastObject = {
-            type: 'space',
-            chars: []
-        };
-
-        lastObject.chars.push({
-            char: ".",
-            failed: null,
-            failedChar: "",
-        });
-
-        dataObject.push(lastObject);
-
-        return dataObject;
-    };
-
     const removeProgressDataFromContentData = () => {
-        //TODO: call an utility function "typing.js" OR "content.js"
-        contentData.value.forEach(cd => {
-            cd.chars.forEach(ch => {
-                ch.failed = null;
-                ch.failedChar = "";
-            });
-        });
+        removeProgressData(contentData.value);
     };
 
     return {contentData, loadDemoContent, loadContentFromCustomFile, removeProgressDataFromContentData};
---
+++ src/utilities/content.js
@@ -0,0 +1,88 @@
+export const isLetter = (str) => {
+    return str.length === 1 && str.match(/[a-z]/i);
+};
+
+export const getContentType = (char) => {
+    let type = "anychar";
+    if(isLetter(char) !== null) {
+        type = "letter";
+    }
+    if(char === '\r') {
+        type = "none";
+    }
+    if(char === '\n') {
+        type = "lf";
+    };
+    if(char === ' ' || char === '\t') {
+        type = "space";
+    };
+    return type;
+};
+
+export const generateContentData = (contentString) => {
+    let dataObject = [];
+    let lastContentType = null;
+    let contentBlock = {};
+   
+    for(let i = 0; i<contentString.length;i++) {
+        let char = contentString[i];
+        let type = getContentType(char);
+
+        char = type === 'anychar' ? char
+            :  type === 'letter' ? char
+            : type === 'space' ? '_' : '';
+            
+        if(i === 0 || lastContentType !== type) {
+            if(i !== 0) {
+                dataObject.push(contentBlock);
+            }
+            lastContentType = type;
+
+            contentBlock = {
+                type: type,
+                chars: []
+            };
+
+            contentBlock.chars.push({
+                char: char,
+                failed: null,
+                failedChar: "",
+            });
+
+        } else {
+            lastContentType = type;
+            contentBlock.chars.push({
+                char: char,
+                failed: null,
+                failedChar: "",
+            });
+        }
+    }
+
+    dataObject = dataObject.filter(item => item.type != 'none');
+    
+    //TODO: find better solution for last char to trigger finish typing
+    let lastObject = {
+        type: 'space',
+        chars: []
+    };
+
+    lastObject.chars.push({
+        char: ".",
+        failed: null,
+        failedChar: "",
+    });
+
+    dataObject.push(lastObject);
+
+    return dataObject;
+};
+
+export const removeProgressData = (contentData) => {
+    contentData.forEach(cd => {
+        cd.chars.forEach(ch => {
+            ch.failed = null;
+            ch.failedChar = "";
+        });
+    });
+};
