6bebce32d8ef12c68663c59fed76378961b54f57
Henry Voigt <oigty69@freenet.de>
2024-04-17 00:51:39 +0200

    update unit test for components/Statepanel

--- tests/unit/components/StatePanel.spec.js
+++ tests/unit/components/StatePanel.spec.js
@@ -4,9 +4,25 @@ import { shallowMount, } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 
 import StatePanel from "@/components/StatePanel.vue";
+
+import { useUiStore } from '@/stores/ui.js';
 import { useAppControllerStore  } from '@/stores/appController';
 
 describe("components/StatePanel.vue", () => {
+    test("whether box height style corresponds uiStore.heightPanel", async () => {
+        const pinia = createTestingPinia();
+        const uiStore = useUiStore(pinia);
+
+        uiStore.heightPanel = 250;
+
+        const wrapper = shallowMount(StatePanel, {
+            global: {
+                plugins: [ pinia ],
+              },
+        });
+        expect(wrapper.findAll('div').at(0).attributes().style).toBe('height: 250px;');
+    });
+
     test("whether displayed 'accuracy' and 'speed of typing' correctly formatted", async () => {
         const pinia = createTestingPinia();
         const appControllerStore = useAppControllerStore(pinia);
@@ -18,7 +34,6 @@ describe("components/StatePanel.vue", () => {
                 plugins: [ pinia ],
               },
         });
-
         expect(wrapper.findAll(".text-h5").at(0).text()).toBe("speed of typing: 68");
         expect(wrapper.findAll(".text-h5").at(1).text()).toBe("accuracy: 12.32 %");
     });
